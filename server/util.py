83d131fb2013aa30cadab25756d73178199d6e97
mark9559 <mwangimark57@gmail.com>
2024-06-07 21:43:42 +0300

    Changed artifacts pickle file

--- server/util.py
+++ server/util.py
@@ -6,7 +6,7 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
+def get_estimated_price(location, sqft, bhk, bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -16,15 +16,14 @@ def get_estimated_price(location,sqft,bhk,bath):
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index>=0:
+    if loc_index >= 0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
-
+    return round(__model.predict([x])[0], 2)
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global  __data_columns
+    global __data_columns
     global __locations
 
     with open("./artifacts/columns.json", "r") as f:
@@ -33,20 +32,9 @@ def load_saved_artifacts():
 
     global __model
     if __model is None:
-        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+        with open('./artifacts/bengaluru_house_data.pickle', 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
 def get_location_names():
     return __locations
-
-def get_data_columns():
-    return __data_columns
-
-if __name__ == '__main__':
-    load_saved_artifacts()
-    print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
